7022d4f38c686330b675aaee3b2bdf47342075ef
Marwan <marwan00mr@gmail.com>
2024-01-11 04:32:59 +0200

    helmet

--- myApp.js
+++ myApp.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const helmet = require("helmet");
 const app = express();
+bodyParser = require('body-parser');
 
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+//app.disable("x-powered-by");
 
-
-
-
-
-
+const ninetyDaysInSeconds = 90*24*60*60;
+console.log(ninetyDaysInSeconds);
+// Use Helmet!
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter())
+app.use(helmet.noSniff())
+app.use(helmet.ieNoOpen())
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds}));
 
 
 
@@ -54,6 +62,8 @@ app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
+  console.log(request.body);
+  //console.log(ninetyDaysInSeconds);
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
